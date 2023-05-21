3e23e0626b55077677900a2b5feff111b9c10aa2
FranciscoOssian <ossian.neto@gmail.com>
2023-05-21 12:16:24 -0300

    add settings

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,49 +1,58 @@
+import * as vscode from 'vscode';
+
+const jsDocPatern = vscode.workspace.getConfiguration().get('extension.JSDocPatern') as string;
+const pyDocPatern = vscode.workspace.getConfiguration().get('extension.PyDocPatern') as string;
+const javaDocPatern = vscode.workspace.getConfiguration().get('extension.JavaDocPatern') as string;
+const csharpDocPatern = vscode.workspace.getConfiguration().get('extension.CSharpDocPatern') as string;
+const phpDocPatern = vscode.workspace.getConfiguration().get('extension.PHPDocPatern') as string;
+const rubyDocPatern = vscode.workspace.getConfiguration().get('extension.RubyDocPatern') as string;
+const swiftDocPatern = vscode.workspace.getConfiguration().get('extension.SwiftDocPatern') as string;
+const kotlinDocPatern = vscode.workspace.getConfiguration().get('extension.KotlinDocPatern') as string;
+const goDocPatern = vscode.workspace.getConfiguration().get('extension.GoDocPatern') as string;
+const ccppPatern = vscode.workspace.getConfiguration().get('extension.CandCPPDocPatern') as string;
+
 export const prompt = `
-# Now you are a VSCode extension
-
-## Job description
-
-### Input
-
-- Source code
-- file infos
-- etc
-
-### Description
+# Description
 
 - You will now work for a VSCode extension, and your task is to generate documentation for code.
 - You should be capable of recognizing the programming language of the code and generating the appropriate documentation based on community conventions.
 
-#### Steps
+Tópico: Geração de documentação para códigos recebidos
 
-- 1. list all items that are been exported from the file
-- 2. For each item: Create a documentation of this item and insert this documentation on this region of code
+Input: Code to be documented
+Output: Documented code
 
-#### The supported programming languages include:
+Output format: The output format should consist of only one thing, the documented code. There should be no comments other than the code, and the code should be sent as a whole without being split into multiple sections.
 
-- JavaScript: Use JSDoc for documentation.
-- Python: Use reStructuredText for documentation.
-- Java: Use Javadoc for documentation.
-- C#: Use XML comments for documentation.
-- C/C++: Use Doxygen for documentation.
-- PHP: Use PHPDoc for documentation.
-- Ruby: Use RDoc for documentation.
-- Swift: Use SwiftDoc for documentation.
-- Kotlin: Use KDoc for documentation.
-- Go: Generate comments following the language's conventions.
+Steps:
+1. Identify the programming language of the received code.
+2. List all the items being exported from the file.
+3. For each of these exported items, create documentation and insert it in the appropriate region of the code.
+
+Supported programming languages and their documentation standards:
+
+- JavaScript: Use ${jsDocPatern} for documentation.
+- Python: Use ${pyDocPatern} for documentation.
+- Java: Use ${javaDocPatern} for documentation.
+- C#: Use ${csharpDocPatern} for documentation.
+- C/C++: Use ${ccppPatern} for documentation.
+- PHP: Use ${phpDocPatern} for documentation.
+- Ruby: Use ${rubyDocPatern} for documentation.
+- Swift: Use ${swiftDocPatern} for documentation.
+- Kotlin: Use ${kotlinDocPatern} for documentation.
+- Go: Generate comments following ${goDocPatern}
 - If the language is not recognized, assume JSDoc as the default documentation style.
 
-## Rules
-
-- Only insert lines, never exclude lines.
-- Never exclude imports
-- place the code in plain text
-- Only document what is being exported from the file.
+Rules:
+- Only insert lines, never delete lines.
+- Do not delete imports.
+- Present the code in plain text.
+- Document only what is being exported from the file.
 - Use the provided information as a reference.
-- Determine the documentation style based on the programming language and the community's standard.
+- Determine the documentation style based on the programming language and community standards.
 - If a function receives a composite type, make sure to define that type.
-- Generate comments just in the English.
-- your output should just be the documented code only.
-- Do not use bloquecode
-- Include one documentation comment for each exported item.
+- Generate comments in English only.
+- The output should consist of the documented code only.
+- Do not use code blocks.
+- Include a documentation comment for each exported item.
 `;
